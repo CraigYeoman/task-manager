e2ea71e023f228200d9e1d0c4cbe02a9d93c8ae9
CraigYeoman <yeomancraig@gmail.com>
2022-11-09 07:16:46 -0600

    CRUD operations completed

--- app.js
+++ app.js
@@ -5,17 +5,12 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
-
+app.use(express.static("./"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
 
 app.use("/api/v1/tasks", tasks);
-
-app.get("/api/v1/tasks");
 
 const port = 3000;
 
